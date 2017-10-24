8e3b03f05f62a714381298e1a8757cf4d0d03f98
aleksat90 <aleksatopalovic90@gmail.com>
2017-10-24 22:44:33 +0200

    web scrapping added to webapp

--- charts/views.py
+++ charts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import urllib.request
+from bs4 import BeautifulSoup
+import re
 
 # Create your views here.
 
@@ -6,5 +9,32 @@ from django.shortcuts import render
 def chartView(request):
 	print('chart View pokrenut')
 	print('pokrenut view')
-	broj = 300
-	return render(request, 'charts/chart.html', {'broj': broj})
+	gradovi = ['cacak','beograd','smederevo','zlatibor','kopaonik','nis','kraljevo','novi-sad','subotica']
+	temp=[]
+	for i in range(len(gradovi)):
+		tre_temp = citanjeTemperature(gradovi[i])
+		temp.append(tre_temp)
+		print(gradovi[i]+" "+tre_temp)
+
+	print('Citanje zavrseno')
+	return render(request, 'charts/chart.html', {'cacak': temp[0],
+													'beograd': temp[1],
+													'smederevo': temp[2],
+													'zlatibor': temp[3],
+													'kopaonik': temp[4],
+													'nis': temp[5],
+													'kraljevo': temp[6],
+													'novisad': temp[7],
+													'subotica':temp[8]})
+
+
+def citanjeTemperature(grad):
+	quote_page = 'https://www.weather2umbrella.com/weather-forecast-'+grad+'-serbia-en/current'
+	page = urllib.request.urlopen(quote_page)
+	soup = BeautifulSoup(page, 'html.parser')
+	temp = soup.find('div', attrs={'class': 'current_temperature_data hidden-xs'}).text
+	temp=re.findall(r'\d+',temp)
+	return temp[0]
+
+
+# Take out the <div> of name and get its value
